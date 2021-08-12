2bc2bf11134cc86cd39ca3f2652fea7dec73b46b
darecallad <Ashane199080706@hotmail.com.tw>
2021-08-11 17:35:20 -0700

    review

--- src/context/MovieRow.jsx
+++ src/context/MovieRow.jsx
@@ -1,9 +1,20 @@
-import React, { useContext } from "react";
+// import React from "react";
+// import UserContext from "./userContext";
+// import { useContext } from "react";
+
+// function MovieRow(props) {
+//   const currentUser = useContext(UserContext);
+//   return <div>{currentUser.name}</div>;
+// }
+// export default MovieRow;
+
+import React from "react";
 import UserContext from "./userContext";
+import { useContext } from "react";
 
 function MovieRow(props) {
-  const currentUser = useContext(UserContext);
-  return <div>{currentUser.name}</div>;
+  const cureentUser = useContext(UserContext);
+  return <div>{cureentUser.name}</div>;
 }
 
 export default MovieRow;
